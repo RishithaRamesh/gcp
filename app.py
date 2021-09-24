f6329e42b87cd2596fd2feca49a9268e4537ea46
Rishitha Ramesh <46816646+RishithaRamesh@users.noreply.github.com>
2021-09-25 00:48:10 +0530

    .

--- app.py
+++ app.py
@@ -32,6 +32,23 @@ with open("questions_final.json") as file:
 
 from flask import Flask,render_template,request, redirect, url_for
  
+# load trained model
+print("Loading Saved Model...")
+model = keras.models.load_model('chat_model.h5')
+
+# load tokenizer object
+print("Loading Tokenizer Model...")
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+# load label encoder object
+print("Loading Encoder Model...")
+with open('label_encoder.pickle', 'rb') as enc:
+    lbl_encoder = pickle.load(enc)
+
+# parameters
+max_len = 20
+
 app = Flask(__name__)
 arr = []
 
@@ -39,48 +56,25 @@ arr = []
 def index():
     return render_template('index.html')
 
-@app.route('/index',methods=['POST','GET'])
+@app.route('/chat',methods=['POST'])
 def chat():
-    # load trained model
-    print("Loading Saved Model...")
-    model = keras.models.load_model('chat_model.h5')
+    if request.method == 'POST':
+        inp = request.form['Question']
+        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),truncating='post', maxlen=max_len))
 
-    # load tokenizer object
-    print("Loading Tokenizer Model...")
-    with open('tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
+        if np.amax(result) < 0.8:
+            pred = "Sorry I didn't understand!"
+            # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL, "Sorry I didn't understand!")
+        else:
+            tag = lbl_encoder.inverse_transform([np.argmax(result)])
 
-    # load label encoder object
-    print("Loading Encoder Model...")
-    with open('label_encoder.pickle', 'rb') as enc:
-        lbl_encoder = pickle.load(enc)
+            for i in data['FAQ']:
+                if i['tag'] == tag:
+                    pred = np.random.choice(i['responses'])
+                    # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL, np.random.choice(i['responses']))
 
-    # parameters
-    max_len = 20
-    while True:
-        if request.method == 'POST':
-            # print(arr)
-            inp = request.form['Question']
-            # if inp.lower() == "quit":
-            #     break
-            result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),truncating='post', maxlen=max_len))
-
-            if np.amax(result) < 0.8:
-                pred = "ChatBot: Sorry I didn't understand!"
-                # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL, "Sorry I didn't understand!")
-                
-            else:
-                tag = lbl_encoder.inverse_transform([np.argmax(result)])
-                print(np.amax(result))
-                for i in data['FAQ']:
-                    if i['tag'] == tag:
-                        pred = "CHATBOT: "+ np.random.choice(i['responses'])
-                        # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL, np.random.choice(i['responses']))
-
-            # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
-            arr.append([inp, pred])
-            return render_template('index.html',pred=pred,arr=arr)
-            # return redirect(url_for('index',pred=pred,arr=arr),code = 302)
+        # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
+        return {"answer": pred}
 
 if __name__ == '__main__':
     app.run(debug=True)
